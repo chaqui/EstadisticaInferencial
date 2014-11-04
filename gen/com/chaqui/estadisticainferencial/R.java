892fd64f951e4b4729bc7d89d8770c23a859cbe2
Josue <josue.chaqui@gmail.com>
2014-11-04 01:06:21 -0600

    creando menu de inicio

--- gen/com/chaqui/estadisticainferencial/R.java
+++ gen/com/chaqui/estadisticainferencial/R.java
@@ -14,7 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int linearLayout1=0x7f070000;
+        public static final int menu_settings=0x7f070006;
+        public static final int textView1=0x7f070001;
+        public static final int textView2=0x7f070002;
+        public static final int textView3=0x7f070003;
+        public static final int textView4=0x7f070004;
+        public static final int textView5=0x7f070005;
     }
     public static final class layout {
         public static final int activity_inicio=0x7f030000;
@@ -23,7 +29,13 @@ public final class R {
         public static final int activity_inicio=0x7f060000;
     }
     public static final class string {
+        public static final int GraficarNormal=0x7f040006;
+        public static final int GraficarTabla=0x7f040008;
+        public static final int Inferencial=0x7f040005;
+        public static final int Probabilidad=0x7f040004;
+        public static final int RegresionLineal=0x7f040007;
         public static final int app_name=0x7f040000;
+        public static final int bienvenida=0x7f040003;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
     }
