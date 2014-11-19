e9a663c8db19e1a99d0af855143d1b26c630c086
Josue <josue.chaqui@gmail.com>
2014-11-18 18:33:58 -0600

    terminadno y generando apk

---
+++ src/com/chaqui/estadisticainferencial/Herramientas.java
@@ -0,0 +1,45 @@
+package com.chaqui.estadisticainferencial;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+public class Herramientas extends Activity {
+	TextView textView1;
+	TextView textView2;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_herramientas);
+		textView1 = (TextView) this.findViewById(R.id.textView1);
+		textView1.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				Intent con = new Intent();
+				con.setClass(getApplicationContext(), Graficar.class);
+				startActivity(con);
+			}
+		});
+		textView2 = (TextView) this.findViewById(R.id.textView1);
+		textView2.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				Intent con = new Intent();
+				con.setClass(getApplicationContext(), Calculadora.class);
+				startActivity(con);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_herramientas, menu);
+		return true;
+	}
+
+}
--- src/com/chaqui/estadisticainferencial/Inicio.java
+++ src/com/chaqui/estadisticainferencial/Inicio.java
@@ -2,63 +2,45 @@ package com.chaqui.estadisticainferencial;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.TextView;
-public class Inicio extends Activity {
-	private TextView GrafCurva;
-	private TextView TextView1;
-	private TextView TextView2;
-	private TextView TextView5;
+import android.widget.TabHost;
+
+public class Inicio extends TabActivity {
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+		 super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_inicio);
-		 GrafCurva = (TextView) this.findViewById(R.id.GrafCurva);
-		 GrafCurva.setOnClickListener(new OnClickListener(){
-				@Override
-				public void onClick(View v) {
-					Intent con = new Intent();
-					con.setClass(getApplicationContext(), Graficar.class);
-					startActivity(con);
-				}
-			});
-		 TextView1 = (TextView) this.findViewById(R.id.textView1);
-		 TextView1.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				Intent con = new Intent();
-				con.setClass(getApplicationContext(), MenuEstadisticaBasica.class);
-				startActivity(con);
-				
-			}
-		});
-		 TextView2 = (TextView) this.findViewById(R.id.textView2);
-		 TextView2.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				Intent con = new Intent();
-				con.setClass(getApplicationContext(), MenuEstadisticaInferencial.class);
-				startActivity(con);
-				
-			}
-		});
-		 TextView5 = (TextView) this.findViewById(R.id.textView5);
-		 TextView5.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				Intent con = new Intent();
-				con.setClass(getApplicationContext(), RegresionLineal.class);
-				startActivity(con);
-				
-			}
-		});
+		TabHost tabs= getTabHost();
+		tabs.setup();
+		
+		Intent intent	 = new Intent().setClass(this, MenuEstadisticaBasica.class);
+		TabHost.TabSpec spec = tabs.newTabSpec("Probabilidad");
+		spec.setIndicator("Probabilidad");
+		spec.setContent(intent);
+		tabs.addTab(spec);
+		
+		intent = new Intent().setClass(this, MenuEstadisticaInferencial.class);
+		spec = tabs.newTabSpec("Inferencial");
+		spec.setIndicator("Inferencial");
+		spec.setContent(intent);
+		tabs.addTab(spec);
+		
+		intent = new Intent().setClass(this, EstadisticaDescriptiva.class);
+		spec = tabs.newTabSpec("Descriptiva");
+		spec.setIndicator("Descriptiva");
+		spec.setContent(intent);
+		tabs.addTab(spec);
+		
+		intent = new Intent().setClass(this, Herramientas.class);
+		spec = tabs.newTabSpec("Herramientas");
+
+		spec.setIndicator("Herramientas");
+		spec.setContent(intent);
+		tabs.addTab(spec);
 		
 	}
 	@Override
